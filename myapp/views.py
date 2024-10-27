1ed3c3276111a0f56daa60368e1107224a25366c
MgZayarNaing <zayarnaing.st@gmail.com>
2024-10-27 13:20:31 +0630

    translate

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+from django.utils.translation import get_language #translate
+def Category_List(request):
+    category = Category.objects.all().order_by('-id')
+    context = {
+        'category':category,
+        'LANGUAGE_CODE': get_language(),
+    }
+    return render(request, 'index.html', context)
